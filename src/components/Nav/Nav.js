f8292de2dcef7fa5525306f4448519fe8bff1d14
philipdaveby <73835849+philipdaveby@users.noreply.github.com>
2021-09-15 11:28:39 +0200

    position of footer and shows only when clicked

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -5,19 +5,21 @@ const Nav = () => {
 
     const [clickedNav, setClickedNav] = useState(false);
 
-    // const clickNav = bar => {
-    //     bar.classList.toggle('change');
-    // }
-    
-    // const toggleClass = () => {
-
-    // }
     return (
         <nav className="nav" >
-            <div className={clickedNav ? 'nav__hamburger change' : 'nav__hamburger'} onClick={() => clickedNav ? setClickedNav(false) : setClickedNav(true)}>
-                <div className="nav__bar1"></div>
-                <div className="nav__bar2"></div>
-                <div className="nav__bar3"></div>
+            <div className="nav__hamburger">
+                {clickedNav ? <div className="links">
+                    <a href="/">PROJECTS</a>
+                    <a href="/">HOME</a>
+                    <a href="/">ABOUT</a>
+                    <a href="/">CONTACT</a>
+                </div> : ''}
+                
+                <div className={clickedNav ? 'nav__animation change' : 'nav__animation'} onClick={() => clickedNav ? setClickedNav(false) : setClickedNav(true)} >
+                    <div className="nav__bar1"></div>
+                    <div className="nav__bar2"></div>
+                    <div className="nav__bar3"></div>
+                </div>
             </div>
         </nav>
     )
