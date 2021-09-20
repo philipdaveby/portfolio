5277300eb689aa9d0fe8f1b32d57524fd014bddd
philipdaveby <73835849+philipdaveby@users.noreply.github.com>
2021-09-20 16:05:01 +0200

    added links

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,6 +3,8 @@ import React from 'react'
 // import Nav from '../Nav/Nav'
 import './home.scss'
 import eatsy from '../../images/eatsy-plan.png'
+import github from '../../images/github.png'
+import website from '../../images/website.png'
 
 const Home = () => {
     return (
@@ -12,6 +14,11 @@ const Home = () => {
                 <p className="arrow-left">&lt;</p>
                 <img src={eatsy} alt="Screenshot of Eatsy app" className="eatsy-screenshot"/>
                 <p className="arrow-right">&gt;</p>
+                <h2 className="home__heading">EATSY</h2>
+                <div className="home__links">
+                    <a href="https://github.com" target="_blank" rel="noopener noreferrer"><img src={github} alt="GitHub logo" className="home__github" /></a>
+                    <a href="https://eatsy.tk" target="_blank" rel="noopener noreferrer"><img src={website} alt="Website logo" className="home__website" /></a>
+                </div>
             </div>
             {/* <Navigation /> */}
             {/* <Nav /> */}
