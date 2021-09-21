4a0ad0b56683de797c5be4e4f34c3170ce5963e4
philipdaveby <73835849+philipdaveby@users.noreply.github.com>
2021-09-21 12:26:14 +0200

    added simple contact site

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,7 +4,11 @@ import './contact.scss'
 const Contact = () => {
     return (
         <div>
-            
+            <p>Say hi!</p>
+            <a href="mailto:philip.daveby@gmail.com">Send an email</a>
+            <p>+46 702 17 28 26</p>
+            <a href="https://www.linkedin.com/in/philipdaveby/">LinkedIn</a>
+            <a href="https://github.com/philipdaveby">GitHub</a>
         </div>
     )
 }
