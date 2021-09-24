5aae89a2bdd3c002823e8c7d70a40f530b019734
philipdaveby <73835849+philipdaveby@users.noreply.github.com>
2021-09-24 14:55:19 +0200

    added text to about and made navbar slightly larger

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -31,6 +31,7 @@ const About = () => {
             {/* <h1>Philip Daveby</h1> */}
             <h2>Fullstack Javascript Developer</h2>
             <p className="about__text">Former professional freelance musician in Stockholm. Driven by learning and passionate about frontend, tech & music. I love solving problems, leading teams and seeing the bigger picture.</p>
+            <p className="about__text">I discovered coding as a teenager but put it aside for my career as a saxophone player. After 11 years of freelancing I wanted to evolve and learn more. I took a year off studying code by myself before I joined the &lt;/salt&gt; bootcamp.</p>
             <h2 className="about__tech-header">Frontend</h2>
             <ul className="about__tech">
                 <li className="about__tech-li"><img className="about__tech-icons" src={javascript} alt="Javascript logo" />
